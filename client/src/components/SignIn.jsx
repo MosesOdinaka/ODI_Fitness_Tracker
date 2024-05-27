55fa2d6ce62c64bcea77355431fe209df3df957c
root <root@DESKTOP-SK90JMN>
2024-05-27 02:43:28 +0100

    feat: Add SignIn component with styled layout and authentication functionality

---
+++ client/src/components/SignIn.jsx
@@ -0,0 +1,120 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import TextInput from "./TextInput";
+import Button from "./Button";
+import { UserSignIn } from "../api";
+import { useDispatch } from "react-redux";
+import { loginSuccess } from "../redux/reducers/userSlice";
+
+/**
+ * Container: Styled component for the main layout of the sign-in form.
+ * Sets the maximum width, and uses flexbox for layout with a column direction.
+ */
+const Container = styled.div`
+  width: 100%;
+  max-width: 500px;
+  display: flex;
+  flex-direction: column;
+  gap: 36px;
+`;
+
+/**
+ * Title: Styled component for the title text.
+ * Uses large font size and bold weight with theme-based primary text color.
+ */
+const Title = styled.div`
+  font-size: 30px;
+  font-weight: 800;
+  color: ${({ theme }) => theme.text_primary};
+`;
+
+/**
+ * Span: Styled component for the subtitle text.
+ * Uses medium font size and regular weight with slightly transparent secondary text color.
+ */
+const Span = styled.div`
+  font-size: 16px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.text_secondary + 90};
+`;
+
+/**
+ * SignIn: Functional component for the sign-in form.
+ * Manages form state and handles user sign-in process.
+ * - Uses `useState` for local component state.
+ * - Uses `useDispatch` from Redux to dispatch login success action.
+ * - Validates input fields before submitting.
+ * - Handles sign-in process with API call and updates UI state accordingly.
+ */
+const SignIn = () => {
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(false);
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const validateInputs = () => {
+    if (!email || !password) {
+      alert("Please fill in all fields");
+      return false;
+    }
+    return true;
+  };
+
+  const handelSignIn = async () => {
+    setLoading(true);
+    setButtonDisabled(true);
+    if (validateInputs()) {
+      await UserSignIn({ email, password })
+        .then((res) => {
+          dispatch(loginSuccess(res.data));
+          alert("Login Success");
+          setLoading(false);
+          setButtonDisabled(false);
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+          setLoading(false);
+          setButtonDisabled(false);
+        });
+    }
+  };
+
+  return (
+    <Container>
+      <div>
+        <Title>Welcome to Fittrack 👋</Title>
+        <Span>Please login with your details here</Span>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          gap: "20px",
+          flexDirection: "column",
+        }}
+      >
+        <TextInput
+          label="Email Address"
+          placeholder="Enter your email address"
+          value={email}
+          handelChange={(e) => setEmail(e.target.value)}
+        />
+        <TextInput
+          label="Password"
+          placeholder="Enter your password"
+          password
+          value={password}
+          handelChange={(e) => setPassword(e.target.value)}
+        />
+        <Button
+          text="SignIn"
+          onClick={handelSignIn}
+          isLoading={loading}
+          isDisabled={buttonDisabled}
+        />
+      </div>
+    </Container>
+  );
+};
+
+export default SignIn;
