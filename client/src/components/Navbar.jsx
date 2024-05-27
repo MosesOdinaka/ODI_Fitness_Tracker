dca091f11779e0865c4534098b644f868a5b6907
root <root@DESKTOP-SK90JMN>
2024-05-27 03:28:51 +0100

    feat: Add Navbar component with styled layout for navigation bar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,222 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import LogoImg from "../utils/Images/Logo.png";
+import { Link as LinkR, NavLink } from "react-router-dom";
+import { MenuRounded } from "@mui/icons-material";
+import { Avatar } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { logout } from "../redux/reducers/userSlice";
+
+/**
+ * Nav: Styled component for the navigation bar.
+ * Sets background color, height, flexbox layout, sticky position, and border bottom.
+ */
+const Nav = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1rem;
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  color: white;
+  border-bottom: 1px solid ${({ theme }) => theme.text_secondary + 20};
+`;
+
+/**
+ * NavContainer: Styled component for the container inside the navigation bar.
+ * Uses flexbox layout to align and justify content, and sets max-width and padding.
+ */
+const NavContainer = styled.div`
+  width: 100%;
+  max-width: 1400px;
+  padding: 0 24px;
+  display: flex;
+  gap: 14px;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 1rem;
+`;
+
+/**
+ * NavLogo: Styled component for the navigation logo link.
+ * Uses flexbox layout to align items and sets font properties and text decoration.
+ */
+const NavLogo = styled(LinkR)`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  gap: 16px;
+  padding: 0 6px;
+  font-weight: 600;
+  font-size: 18px;
+  text-decoration: none;
+  color: ${({ theme }) => theme.black};
+`;
+
+/**
+ * Logo: Styled component for the logo image.
+ * Sets the height of the logo.
+ */
+const Logo = styled.img`
+  height: 42px;
+`;
+
+/**
+ * Mobileicon: Styled component for the mobile menu icon.
+ * Displays only on screens smaller than 768px.
+ */
+const Mobileicon = styled.div`
+  color: ${({ theme }) => theme.text_primary};
+  display: none;
+  @media screen and (max-width: 768px) {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+/**
+ * NavItems: Styled component for the navigation items list.
+ * Uses flexbox layout to align and justify items, and hides on screens smaller than 768px.
+ */
+const NavItems = styled.ul`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 32px;
+  padding: 0 6px;
+  list-style: none;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+/**
+ * Navlink: Styled component for the individual navigation links.
+ * Sets font properties, hover effects, and active link styles.
+ */
+const Navlink = styled(NavLink)`
+  display: flex;
+  align-items: center;
+  color: ${({ theme }) => theme.text_primary};
+  font-weight: 500;
+  cursor: pointer;
+  transition: all 1s slide-in;
+  text-decoration: none;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+  }
+  &.active {
+    color: ${({ theme }) => theme.primary};
+    border-bottom: 1.8px solid ${({ theme }) => theme.primary};
+  }
+`;
+
+/**
+ * UserContainer: Styled component for the user information container.
+ * Uses flexbox layout to align and justify content, and sets padding and gap.
+ */
+const UserContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: flex-end;
+  gap: 16px;
+  align-items: center;
+  padding: 0 6px;
+  color: ${({ theme }) => theme.primary};
+`;
+
+/**
+ * TextButton: Styled component for text buttons.
+ * Sets text alignment, color, cursor, font properties, and hover effects.
+ */
+const TextButton = styled.div`
+  text-align: end;
+  color: ${({ theme }) => theme.secondary};
+  cursor: pointer;
+  font-size: 16px;
+  transition: all 0.3s ease;
+  font-weight: 600;
+  &:hover {
+    color: ${({ theme }) => theme.primary};
+  }
+`;
+
+/**
+ * MobileMenu: Styled component for the mobile menu.
+ * Uses flexbox layout for column direction, and sets styles for position, transition, and visibility.
+ */
+const MobileMenu = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  gap: 16px;
+  padding: 0 6px;
+  list-style: none;
+  width: 90%;
+  padding: 12px 40px 24px 40px;
+  background: ${({ theme }) => theme.bg};
+  position: absolute;
+  top: 80px;
+  right: 0;
+  transition: all 0.6s ease-in-out;
+  transform: ${({ isOpen }) =>
+    isOpen ? "translateY(0)" : "translateY(-100%)"};
+  border-radius: 0 0 20px 20px;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+  opacity: ${({ isOpen }) => (isOpen ? "100%" : "0")};
+  z-index: ${({ isOpen }) => (isOpen ? "1000" : "-1000")};
+`;
+
+/**
+ * Navbar: Functional component for the navigation bar.
+ * @param {Object} props - Component props.
+ * @param {Object} props.currentUser - Current logged-in user information.
+ * Manages the state of the mobile menu and handles user logout.
+ * Renders the navigation bar with logo, links, and user information.
+ */
+const Navbar = ({ currentUser }) => {
+  const dispatch = useDispatch();
+  const [isOpen, setisOpen] = useState(false);
+  return (
+    <Nav>
+      <NavContainer>
+        <Mobileicon onClick={() => setisOpen(!isOpen)}>
+          <MenuRounded sx={{ color: "inherit" }} />
+        </Mobileicon>
+        <NavLogo to="/">
+          <Logo src={LogoImg} />
+          Fittrack
+        </NavLogo>
+
+        <MobileMenu isOpen={isOpen}>
+          <Navlink to="/">Dashboard</Navlink>
+          <Navlink to="/workouts">Workouts</Navlink>
+          <Navlink to="/tutorials">Tutorials</Navlink>
+          <Navlink to="/blogs">Blogs</Navlink>
+          <Navlink to="/contact">Contact</Navlink>
+        </MobileMenu>
+
+        <NavItems>
+          <Navlink to="/">Dashboard</Navlink>
+          <Navlink to="/workouts">Workouts</Navlink>
+          <Navlink to="/tutorials">Tutorials</Navlink>
+          <Navlink to="/blogs">Blogs</Navlink>
+          <Navlink to="/contact">Contact</Navlink>
+        </NavItems>
+
+        <UserContainer>
+          <Avatar src={currentUser?.img}>{currentUser?.name[0]}</Avatar>
+          <TextButton onClick={() => dispatch(logout())}>Logout</TextButton>
+        </UserContainer>
+      </NavContainer>
+    </Nav>
+  );
+};
+
+export default Navbar;
