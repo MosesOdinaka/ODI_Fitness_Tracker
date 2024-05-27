509efda2fcd13831a108f708601ddb50021f2bef
root <root@DESKTOP-SK90JMN>
2024-05-27 02:34:03 +0100

    feat: Add main application structure with theme and routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,56 @@
-import { ThemeProvider, styled } from 'styled-components';
-import {lightTheme} from './utils/Themes';
-import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider, styled } from "styled-components";
+import { lightTheme } from "./utils/Themes";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Authentication from "./pages/Authentication";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import Navbar from "./components/Navbar";
+import Dashboard from "./pages/Dashboard";
+import Workouts from "./pages/Workouts";
 
+/**
+ * Container: Main container for the entire application.
+ * Sets up full height and width with flexbox layout.
+ * Applies background color and text color based on the current theme.
+ * Disables horizontal and vertical overflow and includes a transition effect.
+ */
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
   flex-direction: column;
   background: ${({ theme }) => theme.bg};
-  color: ${({ theme }) => theme.text_primary}
+  color: ${({ theme }) => theme.text_primary};
   overflow-x: hidden;
   overflow-y: hidden;
   transition: all 0.2s ease;
 `;
 
+/**
+ * App: Root component of the application.
+ * Wraps the application in a ThemeProvider and BrowserRouter.
+ * Uses Redux to get the current user state and conditionally renders:
+ * - Navbar and protected routes (Dashboard and Workouts) if the user is logged in.
+ * - Authentication component if the user is not logged in.
+ */
 function App() {
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <ThemeProvider theme={lightTheme}>
       <BrowserRouter>
-        <container>Hi</container>
+        {currentUser ? (
+          <Container>
+            <Navbar currentUser={currentUser} />
+            <Routes>
+              <Route path="/" exact element={<Dashboard />} />
+              <Route path="/workouts" exact element={<Workouts />} />
+            </Routes>
+          </Container>
+        ) : (
+          <Container>
+            <Authentication />
+          </Container>
+        )}
       </BrowserRouter>
     </ThemeProvider>
   );
