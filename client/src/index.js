1fb5070d885c9e38e8c52f16694d430499d30b35
root <root@MOSES.localdomain>
2024-06-06 17:58:18 -0700

    Removed unneccessary doc string.

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 import App from './App';
-import { store, persistor } from './redux/store'; // Adjust the import path as necessary
+import { store, persistor } from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
